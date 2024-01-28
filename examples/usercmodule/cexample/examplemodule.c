e511b906618b5caf0f05852a2bcec9cce7cff252
OwenDelBene <owen.delbene@gmail.com>
2024-01-28 15:20:08 -0600

    Update soh modules
    Updating soh structs with new mainboard telemetry, probably should
    eventually change if/else to case switch for compiler optimization

--- examples/usercmodule/cexample/examplemodule.c
+++ examples/usercmodule/cexample/examplemodule.c
@@ -459,29 +459,102 @@ const mp_obj_type_t powerSoh_type = {
 
 typedef struct _tempSoh_obj_t {
     mp_obj_base_t base;
-    mp_float_t x;
+    mp_float_t battery;
+	mp_float_t ntc1;
+	mp_float_t ntc2;
+	mp_float_t ntc3;
+	mp_float_t ntc4;
+	mp_float_t a;
+	mp_float_t b;
+	mp_float_t c;
+	mp_float_t d;
+	mp_float_t timestamp;
 } tempSoh_obj_t;
 
 const mp_obj_type_t tempSoh_type;
 
 STATIC mp_obj_t tempSoh_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
-    mp_arg_check_num(n_args, n_kw, 1, 1, true);
+    mp_arg_check_num(n_args, n_kw, 10, 10, true);
     tempSoh_obj_t *self = m_new_obj(tempSoh_obj_t);
     self->base.type = &tempSoh_type;
-    self->x = mp_obj_get_float(args[0]);
-    return MP_OBJ_FROM_PTR(self);
+    self->battery = mp_obj_get_float(args[0]);
+    self->ntc1 = mp_obj_get_float(args[1]);
+    self->ntc2 = mp_obj_get_float(args[2]);
+    self->ntc3 = mp_obj_get_float(args[3]);
+    self->ntc4 = mp_obj_get_float(args[4]);
+    self->a = mp_obj_get_float(args[5]);
+    self->b = mp_obj_get_float(args[6]);
+    self->c = mp_obj_get_float(args[7]);
+    self->d = mp_obj_get_float(args[8]);
+    self->timestamp = mp_obj_get_float(args[9]);
+   	 
+	return MP_OBJ_FROM_PTR(self);
 }
 //getter only visible to c interface, called by propertyclass_attr
 //if attr qstr == x's qster
-STATIC mp_obj_t tempSoh_x(mp_obj_t self_in) {
+STATIC mp_obj_t tempSoh_battery(mp_obj_t self_in) {
     tempSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    return mp_obj_new_float(self->x);
+    return mp_obj_new_float(self->battery);
+}
+STATIC mp_obj_t tempSoh_ntc1(mp_obj_t self_in) {
+    tempSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_float(self->ntc1);
+}
+STATIC mp_obj_t tempSoh_ntc2(mp_obj_t self_in) {
+    tempSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_float(self->ntc2);
+}
+STATIC mp_obj_t tempSoh_ntc3(mp_obj_t self_in) {
+    tempSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_float(self->ntc4);
+}
+STATIC mp_obj_t tempSoh_ntc4(mp_obj_t self_in) {
+    tempSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_float(self->ntc4);
+}
+STATIC mp_obj_t tempSoh_a(mp_obj_t self_in) {
+    tempSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_float(self->a);
+}
+STATIC mp_obj_t tempSoh_b(mp_obj_t self_in) {
+    tempSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_float(self->b);
+}
+STATIC mp_obj_t tempSoh_c(mp_obj_t self_in) {
+    tempSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_float(self->c);
+}
+STATIC mp_obj_t tempSoh_d(mp_obj_t self_in) {
+    tempSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_float(self->d);
+}
+STATIC mp_obj_t tempSoh_timestamp(mp_obj_t self_in) {
+    tempSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_float(self->timestamp);
 }
 //define object with one parameter
-MP_DEFINE_CONST_FUN_OBJ_1(tempSoh_x_obj, tempSoh_x);
+MP_DEFINE_CONST_FUN_OBJ_1(tempSoh_battery_obj, tempSoh_battery);
+MP_DEFINE_CONST_FUN_OBJ_1(tempSoh_ntc1_obj, tempSoh_ntc1);
+MP_DEFINE_CONST_FUN_OBJ_1(tempSoh_ntc2_obj, tempSoh_ntc2);
+MP_DEFINE_CONST_FUN_OBJ_1(tempSoh_ntc3_obj, tempSoh_ntc3);
+MP_DEFINE_CONST_FUN_OBJ_1(tempSoh_ntc4_obj, tempSoh_ntc4);
+MP_DEFINE_CONST_FUN_OBJ_1(tempSoh_a_obj, tempSoh_a);
+MP_DEFINE_CONST_FUN_OBJ_1(tempSoh_b_obj, tempSoh_b);
+MP_DEFINE_CONST_FUN_OBJ_1(tempSoh_c_obj, tempSoh_c);
+MP_DEFINE_CONST_FUN_OBJ_1(tempSoh_d_obj, tempSoh_d);
+MP_DEFINE_CONST_FUN_OBJ_1(tempSoh_timestamp_obj, tempSoh_timestamp);
 //map qsttr for attribute to attribute object
 STATIC const mp_rom_map_elem_t tempSoh_locals_dict_table[] = {
-    { MP_ROM_QSTR(MP_QSTR_x), MP_ROM_PTR(&tempSoh_x_obj) },
+    { MP_ROM_QSTR(MP_QSTR_battery), MP_ROM_PTR(&tempSoh_battery_obj) },
+    { MP_ROM_QSTR(MP_QSTR_ntc1), MP_ROM_PTR(&tempSoh_ntc1_obj) },
+    { MP_ROM_QSTR(MP_QSTR_ntc2), MP_ROM_PTR(&tempSoh_ntc2_obj) },
+    { MP_ROM_QSTR(MP_QSTR_ntc3), MP_ROM_PTR(&tempSoh_ntc3_obj) },
+    { MP_ROM_QSTR(MP_QSTR_ntc4), MP_ROM_PTR(&tempSoh_ntc4_obj) },
+    { MP_ROM_QSTR(MP_QSTR_a), MP_ROM_PTR(&tempSoh_a_obj) },
+    { MP_ROM_QSTR(MP_QSTR_b), MP_ROM_PTR(&tempSoh_b_obj) },
+    { MP_ROM_QSTR(MP_QSTR_c), MP_ROM_PTR(&tempSoh_c_obj) },
+    { MP_ROM_QSTR(MP_QSTR_d), MP_ROM_PTR(&tempSoh_d_obj) },
+    { MP_ROM_QSTR(MP_QSTR_timestamp), MP_ROM_PTR(&tempSoh_timestamp_obj) },
 };
 
 STATIC MP_DEFINE_CONST_DICT(tempSoh_locals_dict, tempSoh_locals_dict_table);
@@ -490,8 +563,32 @@ STATIC MP_DEFINE_CONST_DICT(tempSoh_locals_dict, tempSoh_locals_dict_table);
 //checks if attribute is a member of the internal data struct
 //destination[0] is the output, must be mp_obj type
 STATIC void tempSoh_attr(mp_obj_t self_in, qstr attribute, mp_obj_t *destination) {
-    if(attribute == MP_QSTR_x) {
-        destination[0] = tempSoh_x(self_in);
+    if(attribute == MP_QSTR_battery) {
+        destination[0] = tempSoh_battery(self_in);
+    }
+    if(attribute == MP_QSTR_ntc1) {
+        destination[0] = tempSoh_ntc1(self_in);
+    }
+    if(attribute == MP_QSTR_ntc2) {
+        destination[0] = tempSoh_ntc2(self_in);
+    }
+    if(attribute == MP_QSTR_ntc3) {
+        destination[0] = tempSoh_ntc3(self_in);
+    }
+    if(attribute == MP_QSTR_ntc4) {
+        destination[0] = tempSoh_ntc4(self_in);
+    }
+    if(attribute == MP_QSTR_a) {
+        destination[0] = tempSoh_a(self_in);
+    }
+    if(attribute == MP_QSTR_b) {
+        destination[0] = tempSoh_b(self_in);
+    }
+    if(attribute == MP_QSTR_c) {
+        destination[0] = tempSoh_c(self_in);
+    }
+    if(attribute == MP_QSTR_d) {
+        destination[0] = tempSoh_d(self_in);
     }
 }
 //register python object visible to interpreter
@@ -512,30 +609,40 @@ const mp_obj_type_t tempSoh_type = {
 //
 typedef struct _comSoh_obj_t {
     mp_obj_base_t base;
-    mp_float_t rssi;
-	mp_float_t snr;
+    //lora
+   	mp_float_t rssi;
 	mp_int_t txcomplete;
 	mp_int_t rxcomplete;
 	mp_uint_t crc;
 	mp_uint_t flags;
-	mp_float_t codingrate;
+	//iridium
+	mp_uint_t status;
+	mp_int_t signal_quality;
+	mp_float_t location;
+	mp_float_t sys_time;
+	mp_float_t energy;
+		
 	mp_float_t timestamp;
 } comSoh_obj_t;
 
 const mp_obj_type_t comSoh_type;
 
 STATIC mp_obj_t comSoh_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
-    mp_arg_check_num(n_args, n_kw, 8, 8, true);
+    mp_arg_check_num(n_args, n_kw, 10, 10, true);
     comSoh_obj_t *self = m_new_obj(comSoh_obj_t);
     self->base.type = &comSoh_type;
     self->rssi = mp_obj_get_float(args[0]);
-    self->snr= mp_obj_get_float(args[1]);
-    self->txcomplete = mp_obj_get_int(args[2]);
-    self->rxcomplete = mp_obj_get_int(args[3]);
-    self->crc = mp_obj_get_int(args[4]);
-    self->flags = mp_obj_get_int(args[5]);
-    self->codingrate = mp_obj_get_float(args[6]);
-    self->timestamp = mp_obj_get_float(args[7]);
+    self->txcomplete = mp_obj_get_int(args[1]);
+    self->rxcomplete = mp_obj_get_int(args[2]);
+    self->crc = mp_obj_get_int(args[3]);
+    self->flags = mp_obj_get_int(args[4]);
+    
+	self->status = mp_obj_get_int(args[5]);	
+	self->signal_quality = mp_obj_get_int(args[6]);	
+	self->location = mp_obj_get_float(args[7]);	
+	self->sys_time = mp_obj_get_float(args[8]);	
+	
+	self->timestamp = mp_obj_get_float(args[9]);
 	return MP_OBJ_FROM_PTR(self);
 }
 //getter only visible to c interface, called by propertyclass_attr
@@ -543,10 +650,6 @@ STATIC mp_obj_t comSoh_make_new(const mp_obj_type_t *type, size_t n_args, size_t
 STATIC mp_obj_t comSoh_rssi(mp_obj_t self_in) {
     comSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
     return mp_obj_new_float(self->rssi);
-}
-STATIC mp_obj_t comSoh_snr(mp_obj_t self_in) {
-    comSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    return mp_obj_new_float(self->snr);
 }
 STATIC mp_obj_t comSoh_txcomplete(mp_obj_t self_in) {
     comSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
@@ -564,33 +667,59 @@ STATIC mp_obj_t comSoh_flags(mp_obj_t self_in) {
     comSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
     return mp_obj_new_float(self->flags);
 }
-STATIC mp_obj_t comSoh_codingrate(mp_obj_t self_in) {
+STATIC mp_obj_t comSoh_status(mp_obj_t self_in) {
     comSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    return mp_obj_new_float(self->codingrate);
+    return mp_obj_new_int(self->status);
 }
+STATIC mp_obj_t comSoh_signal_quality(mp_obj_t self_in) {
+    comSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_int(self->signal_quality);
+}
+STATIC mp_obj_t comSoh_location(mp_obj_t self_in) {
+    comSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_float(self->location);
+}
+STATIC mp_obj_t comSoh_sys_time(mp_obj_t self_in) {
+    comSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_float(self->sys_time);
+}
+STATIC mp_obj_t comSoh_energy(mp_obj_t self_in) {
+    comSoh_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_float(self->energy);
+}
+
+
 STATIC mp_obj_t comSoh_timestamp(mp_obj_t self_in) {
 	comSoh_obj_t* self = MP_OBJ_TO_PTR(self_in);
 	return mp_obj_new_float(self->timestamp);
 }
 //define object with one parameter
 MP_DEFINE_CONST_FUN_OBJ_1(comSoh_rssi_obj, comSoh_rssi);
-MP_DEFINE_CONST_FUN_OBJ_1(comSoh_snr_obj, comSoh_snr);
 MP_DEFINE_CONST_FUN_OBJ_1(comSoh_txcomplete_obj, comSoh_txcomplete);
 MP_DEFINE_CONST_FUN_OBJ_1(comSoh_rxcomplete_obj, comSoh_rxcomplete);
 MP_DEFINE_CONST_FUN_OBJ_1(comSoh_crc_obj, comSoh_crc);
 MP_DEFINE_CONST_FUN_OBJ_1(comSoh_flags_obj, comSoh_flags);
-MP_DEFINE_CONST_FUN_OBJ_1(comSoh_codingrate_obj, comSoh_codingrate);
+MP_DEFINE_CONST_FUN_OBJ_1(comSoh_status_obj, comSoh_status);
+MP_DEFINE_CONST_FUN_OBJ_1(comSoh_signal_quality_obj, comSoh_signal_quality);
+MP_DEFINE_CONST_FUN_OBJ_1(comSoh_location_obj, comSoh_location);
+MP_DEFINE_CONST_FUN_OBJ_1(comSoh_sys_time_obj, comSoh_sys_time);
+MP_DEFINE_CONST_FUN_OBJ_1(comSoh_energy_obj, comSoh_energy);
 MP_DEFINE_CONST_FUN_OBJ_1(comSoh_timestamp_obj, comSoh_timestamp);
 //map qsttr for attribute to attribute object
 STATIC const mp_rom_map_elem_t comSoh_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_rssi), MP_ROM_PTR(&comSoh_rssi_obj) },
-    { MP_ROM_QSTR(MP_QSTR_snr), MP_ROM_PTR(&comSoh_snr_obj) },
     { MP_ROM_QSTR(MP_QSTR_txcomplete), MP_ROM_PTR(&comSoh_txcomplete_obj) },
     { MP_ROM_QSTR(MP_QSTR_rxcomplete), MP_ROM_PTR(&comSoh_rxcomplete_obj) },
     { MP_ROM_QSTR(MP_QSTR_crc), MP_ROM_PTR(&comSoh_crc_obj) },
     { MP_ROM_QSTR(MP_QSTR_flags), MP_ROM_PTR(&comSoh_flags_obj) },
-    { MP_ROM_QSTR(MP_QSTR_codingrate), MP_ROM_PTR(&comSoh_codingrate_obj) },
-    { MP_ROM_QSTR(MP_QSTR_timestamp), MP_ROM_PTR(&comSoh_timestamp_obj) },
+    
+    { MP_ROM_QSTR(MP_QSTR_status), MP_ROM_PTR(&comSoh_status_obj) },
+    { MP_ROM_QSTR(MP_QSTR_signal_quality), MP_ROM_PTR(&comSoh_signal_quality_obj) },
+    { MP_ROM_QSTR(MP_QSTR_location), MP_ROM_PTR(&comSoh_location_obj) },
+    { MP_ROM_QSTR(MP_QSTR_sys_time), MP_ROM_PTR(&comSoh_sys_time_obj) },
+    { MP_ROM_QSTR(MP_QSTR_energy), MP_ROM_PTR(&comSoh_energy_obj) },
+	
+	{ MP_ROM_QSTR(MP_QSTR_timestamp), MP_ROM_PTR(&comSoh_timestamp_obj) },
 };
 
 STATIC MP_DEFINE_CONST_DICT(comSoh_locals_dict, comSoh_locals_dict_table);
@@ -601,9 +730,6 @@ STATIC MP_DEFINE_CONST_DICT(comSoh_locals_dict, comSoh_locals_dict_table);
 STATIC void comSoh_attr(mp_obj_t self_in, qstr attribute, mp_obj_t *destination) {
     if(attribute == MP_QSTR_rssi) {
         destination[0] = comSoh_rssi(self_in);
-    }
-    if(attribute == MP_QSTR_snr) {
-        destination[0] = comSoh_snr(self_in);
     }
     if(attribute == MP_QSTR_txcomplete) {
         destination[0] = comSoh_txcomplete(self_in);
@@ -617,12 +743,27 @@ STATIC void comSoh_attr(mp_obj_t self_in, qstr attribute, mp_obj_t *destination)
     if(attribute == MP_QSTR_flags) {
         destination[0] = comSoh_flags(self_in);
     }
-    if(attribute == MP_QSTR_codingrate) {
-        destination[0] = comSoh_codingrate(self_in);
+	
+    if(attribute == MP_QSTR_status) {
+        destination[0] = comSoh_status(self_in);
     }
+    if(attribute == MP_QSTR_signal_quality) {
+        destination[0] = comSoh_signal_quality(self_in);
+    }
+    if(attribute == MP_QSTR_location) {
+        destination[0] = comSoh_location(self_in);
+    }
+    if(attribute == MP_QSTR_sys_time) {
+        destination[0] = comSoh_sys_time(self_in);
+    }
+    if(attribute == MP_QSTR_energy) {
+        destination[0] = comSoh_energy(self_in);
+    }
+	
 	if (attribute == MP_QSTR_timestamp) {
 		destination[0] = comSoh_timestamp(self_in);
 	}
+
 }
 
 
@@ -636,8 +777,6 @@ STATIC void comSoh_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kin
     mp_obj_print_helper(print, mp_obj_new_float(self->rssi), PRINT_REPR);
 	
 	
-	mp_print_str(print, ", snr: ");
-    mp_obj_print_helper(print, mp_obj_new_float(self->snr), PRINT_REPR);
 	
 	mp_print_str(print, ", txcomplete: ");
     mp_obj_print_helper(print,mp_obj_new_int(self->txcomplete), PRINT_REPR);
@@ -652,8 +791,6 @@ STATIC void comSoh_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kin
 	mp_print_str(print, ", flags: ");
     mp_obj_print_helper(print, mp_obj_new_int(self->rxcomplete), PRINT_REPR);
 	
-	mp_print_str(print, ", codingrate: ");
-    mp_obj_print_helper(print, mp_obj_new_float(self->rxcomplete), PRINT_REPR);
 	
 	mp_print_str(print, ", timestamp: ");
     mp_obj_print_helper(print, mp_obj_new_float(self->timestamp), PRINT_REPR);
