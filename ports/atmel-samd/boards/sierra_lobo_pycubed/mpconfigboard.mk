2b9d3fef6d2c58d91ecda27fe0253eea5a8ec819
Caden Hillis <caden.j.hillis@sierralobo.com>
2024-03-11 19:24:58 -0400

    added frozen modules to build

--- ports/atmel-samd/boards/sierra_lobo_pycubed/mpconfigboard.mk
+++ ports/atmel-samd/boards/sierra_lobo_pycubed/mpconfigboard.mk
@@ -13,7 +13,7 @@ LONGINT_IMPL = MPZ
 
 CIRCUITPY_FULL_BUILD = 1
 
-CIRCUITPY_ASYNCIO = 1
+#CIRCUITPY_ASYNCIO = 1
 CIRCUITPY_ATEXIT = 1
 CIRCUITPY_OS_GETENV = 1
 CIRCUITPY_TRACEBACK = 1
@@ -53,9 +53,13 @@ CIRCUITPY_ROTARYIO = 0
 CIRCUITPY_COUNTIO = 0
 CIRCUITPY_GIFIO = 0
 
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_GPS
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Register
 FROZEN_MPY_DIRS += $(TOP)/frozen/adcs
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_GPS
+FROZEN_MPY_DIRS += $(TOP)/frozen/SierraLobo_CircuitPython_DS134x
+FROZEN_MPY_DIRS += $(TOP)/frozen/SierraLobo_CircuitPython_IAM20380
+FROZEN_MPY_DIRS += $(TOP)/frozen/SierraLobo_CircuitPython_INA230
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_MMC56x3
+FROZEN_MPY_DIRS += $(TOP)/frozen/mtdb
 FROZEN_MPY_DIRS += $(TOP)/frozen/pycubed_rfm9x
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_RockBlock
